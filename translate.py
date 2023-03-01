1c3ffabd1f589355b3417599dc0ab52caaf53b63
琪露诺酱 <46920034+Rcrwrate@users.noreply.github.com>
2023-03-01 03:07:40 +0000

    INIT

---
+++ translate.py
@@ -0,0 +1,29 @@
+from Lib.Translate import Bing
+from Lib.Network import Network
+from flask import Flask, request
+
+
+# s = Network({},log_level=10)
+B = Bing()
+B.get_token()
+
+to = "zh-Hans"
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    text = request.args["text"]
+    try:
+        int(text)
+        return text
+    except:
+        pass
+    try:
+        return B.translate(text, to)
+    except:
+        B.get_token()
+        return B.translate(text, to)
+
+
+app.run(port=9000, host='0.0.0.0')
