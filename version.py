152ddae2d0a0b0c0052d5b15139022066cad978c
琪露诺酱 <46920034+Rcrwrate@users.noreply.github.com>
2023-03-07 03:01:53 +0000

    反了反了.jpg

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
 import json
+from filehash import filehash
 
 ver = {
     "version": 0.5,
@@ -6,8 +7,12 @@ ver = {
     "msg": "发现新的更新咯，点击前往更新界面\nhttps://rcrwrate.github.io/Limbus-Company-zh_CN/\n\n快速安装\nhttps://limbus_company.lanzoul.com/b00wpywsh 密码: go0b\n\n",
     "forcemsg": "本次为强制更新，不更新会严重影响使用",
     "file": {
-        "update.py": 1.0,
-        "translate.py": 1.1
+        "update.py": 1.1,
+        "translate.py": 1.0
+    },
+    "filehath": {
+        "update.py": filehash("update.py"),
+        "translate.py": filehash("translate.py")
     }
 }
 
